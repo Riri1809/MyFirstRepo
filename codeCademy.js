1ddcfdebbe9f9c1e27c56d0c711b81400587f7fb
Rachi-Issa <133571752+Riri1809@users.noreply.github.com>
2023-12-21 19:58:10 -0500

    Update codeCademy.js

--- codeCademy.js
+++ codeCademy.js
@@ -13,3 +13,40 @@ if (season === 'spring') {
 else {
   console.log('Invalid season.');
 }
+//eightBall game
+userName = '';
+userName ==='Jane' ? console.log('Hello, Jane') : console.log('Hello!'); 
+const userQuestion ='what do you like?';
+console.log(userQuestion);
+const randomNumber = Math.floor(Math.random() * 8);
+console.log(randomNumber);
+let eightBall = '';
+switch(randomNumber){
+  case 0:
+  eightBall = 'It is certain';
+  break;
+  case 1:
+  eightBall = 'It is decidedly so';
+  break;
+  case 2:
+  eightBall = 'Reply hazy try again';
+  break;
+  case 3:
+  eightBall = 'Cannot predict now';
+  break;
+  case 4:
+  eightBall = 'Do not count on it';
+  break;
+  case 5:
+  eightBall = 'My sources say no';
+  case 6:
+  eightBall = 'Outlook not so good';
+  break;
+  case 7:
+  eightBall = 'Signs point to yes';
+  break;
+  default:
+  eightBall = 'answer unavailable';
+  break;
+}
+console.log(eightBall);
